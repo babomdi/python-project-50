8003285c835f67d46f192200ddf554c1f905186d
babomdi <dasha.ryzhikova@bk.ru>
2023-11-02 14:38:37 +0300

    fix linter

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -47,8 +47,7 @@ def convert_to_stylish(value, replacer=SEPARATOR, spaces_count=2):
                     f"{indent+DELETE}{key}: {walk(d['value'], depth+1)}")
             elif action == 'modified':
                 lines.append(
-                    f"{indent+DELETE}{key}: {walk(d['old_value'], depth+1)}"
-                        )
+                    f"{indent+DELETE}{key}: {walk(d['old_value'], depth+1)}")
                 lines.append(
                     f"{indent+ADD}{key}: {walk(d['new_value'], depth+1)}")
             else:
